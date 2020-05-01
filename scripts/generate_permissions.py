81b1ffc5e629377b1acdf15159fb26e0c5c80211
pradumangoyal <goyal.praduman611@gmail.com>
2020-05-01 09:51:14 +0530

    Add script to generate all permissions and silent the notifications

---
+++ scripts/generate_permissions.py
@@ -0,0 +1,49 @@
+import swapper
+
+from no_dues.models import Authority, Subscriber, Permission
+
+Department = swapper.load_model('kernel', 'Department')
+common_slugs = ['lib', 'ccb', 'hbc', 'icc', 'iso', 'amp', 'bad', 'hec']
+ug_slugs = ['nss', 'ncc', 'nso', 'hoc']
+pg_slugs = ['tsp']
+
+
+subscribers = Subscriber.objects.all()
+authorities = Authority.objects.all()
+
+
+def generate_permission(authority_slug, subscriber):
+    student = subscriber.person.student
+
+    try:
+        authority = Authority.objects.get(slug=authority_slug)
+        permission, _ = Permission.objects.get_or_create(
+            subscriber=subscriber, authority=authority)
+    except Exception as e:
+        print(f'{student.enrolment_number}: {authority_slug} {str(e)}')
+
+
+def generate_permissions_for_subscribers(subscribers):
+    for subscriber in subscribers:
+        student = subscriber.person.student
+        department = student.branch.department
+        department_code = department.code
+        if type(department) is Department:
+            slug = '_department'
+        else:
+            slug = '_centre'
+
+        authority_department_slug = f'{department_code}{slug}'
+
+        slugs = list()
+        slugs.append(authority_department_slug)
+        slugs.extend(common_slugs)
+
+        graduation_code = student.branch.degree.graduation[0]
+        if graduation_code == 'gra':
+            slugs.extend(ug_slugs)
+        elif graduation_code == 'doc':
+            slugs.extend(pg_slugs)
+
+        for slug in slugs:
+            generate_permission(slug, subscriber)
--- utils/send_notification.py
+++ utils/send_notification.py
@@ -46,24 +46,28 @@ def send_notification(subject_text, body_text, front_path, to_authority, subscri
 
     full_path = f'https://newchanneli.iitr.ac.in/?next=/{front_path}'
 
-    push_notification(
-        template=subject_text,
-        category=category,
-        web_onclick_url=front_path,
-        android_onclick_activity='',
-        ios_onclick_action='',
-        is_personalised=False,
-        person=None,
-        has_custom_users_target=True,
-        persons=persons,
-    )
+    try:
+        push_notification(
+            template=subject_text,
+            category=category,
+            web_onclick_url=front_path,
+            android_onclick_activity='',
+            ios_onclick_action='',
+            is_personalised=False,
+            person=None,
+            has_custom_users_target=True,
+            persons=persons,
+        )
 
-    email_push(
-        subject_text=subject_text,
-        body_text=body_text,
-        category=category,
-        has_custom_user_target=True,
-        persons=persons,
-        target_app_name=app_verbose_name,
-        target_app_url=full_path,
-    )
+        email_push(
+            subject_text=subject_text,
+            body_text=body_text,
+            category=category,
+            has_custom_user_target=True,
+            persons=persons,
+            target_app_name=app_verbose_name,
+            target_app_url=full_path,
+        )
+
+    except Exception as e:
+        pass
